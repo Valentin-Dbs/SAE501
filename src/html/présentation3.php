93eb9d9adedf64ec57902b1a73eafea9e8f4846c
EmmaCunienq <3hda07@gmail.com>
2023-12-01 18:25:06 +0100

    présentation 3

--- src/html/présentation3.php
+++ src/html/présentation3.php
@@ -33,142 +33,153 @@
         <div style="width: 90%;">
             <div class="container">
                 <button onclick="prevSlide()" class="arrows">
-                    < 
-                </button>
-                <div class="carousel-container">
-                    <div class="carousel-slide">
-                        <img src="..\img\mmi-logo-couleur_1.png" alt="Logo MMI" width="100px">
-                        <h2>Découvrir les débouchés</h2>
-                        <p>
-                            Comme le BUT MMI permet d'acquérir énormément de compétences différentes, il permet aussi
-                            de travailler dans toutes sortes de métiers et toutes sortes de domaines.
-                        </p>
-                        <p>
-                            Pour les créatifs amoureux des graphismes ou de l’audiovisuel, il est tout à fait
-                            possible de travailler dans une agence publicitaire ou de communication.
-                        </p>
-                        <p>
-                            Pour les acharnés du code, votre avenir pourrait se dessiner dans le développement web,
-                            la programmation d’applications, ou même la sécurité informatique.
-                        </p>
-                        <p>
-                            Pour les maîtres de la communication, une carrière pourrait se profiler dans le
-                            marketing, la stratégie de communication, la gestion des médias sociaux.
-                        </p>
-                        <p>
-                            C’est une formation très polyvalente qui permet d’accéder à beaucoup de compétences, il
-                            y a donc une plus grande liberté et diversité de choix que ce soit dans les études
-                            supérieurs ou la voix professionnels.
-                        </p>
-                        <h4>Bonne découverte !</h4>
-                    </div>
-                    <div class="carousel-slide">
-                        <img src="..\img\developpeur-informatique.jpeg" alt="Développeur informatique" width="200px">
-                        <h2>Développeur / développeuse informatique</h2>
-                        <p>
-                            <b>Synonymes :</b> Analyste-programmeur/euse, Ingénieur/e d'études et de développement
-                            <br>
-                            <b>Métiers Associés : </b> Développeur/euse d'applications mobiles, Développeur/euse
-                            multimédia
-                            <br>
-                            <b>Centres d'intérêt :</b> Je me passionne pour les nouvelles technologies, Je suis
-                            accro au
-                            numérique
-                        </p>
-                        <p>
-                            Expert des langages informatiques, le développeur informatique traduit la demande d'un
-                            client en lignes de code informatique. La révolution numérique le place parmi les
-                            professionnels les plus recherchés, surtout s'il sait s'adapter et élargir ses
-                            compétences.
-                        </p>
-                    </div>
-                    <div class="carousel-slide">
-                        <img src="..\img\animateur-2d-3d.jpeg" alt="Animation 2D" width="200px">
-                        <h2>Animateur / animatrice 2D et 3D</h2>
-                        <p>
-                            <b>Synonymes :</b> Animateur/trice de dessin animé, Animateur/trice en images de
-                            synthèse, Infographiste
-                            <br>
-                            <b> Secteurs professionnels :</b> Audiovisuel, Édition, librairie, bibliothèque,
-                            Journalisme
-                            <br>
-                            <b> Centres d'intérêt : </b>J'ai un bon coup de crayon, je me passionne pour les
-                            nouvelles
-                            technologies, je rêve d'un métier artistique, je suis accro au numérique
-                        </p>
-                        <p>
-                            Le travail d'animateur 2D implique de donner vie à des images dessinées sur papier ou
-                            tablette, tandis que l'animateur 3D travaille sur des modèles informatiques pour créer
-                            une illusion de relief et de volume, contribuant ainsi à la réalisation de films
-                            d'animation et de jeux vidéo en collaboration avec d'autres professionnels.
-                        </p>
-                    </div>
-                    <div class="carousel-slide">
-                        <img src="..\img\concepteur-multimedia.jpeg" alt="Image 3" width="200px">
-                        <h2>Concepteur / conceptrice multimédia</h2>
-                        <p>
-                            <b>Synonymes :</b> Concepteur/trice-réalisateur/trice multimédia
-                            <br>
-                            <b>Métiers Associés :</b> Concepteur/trice de jeux vidéo
-                            <br>
-                            <b>Secteurs professionnels :</b> Édition, librairie, bibliothèque, Informatique et
-                            réseaux,
-                            Journalisme, Marketing , publicité
-                            <br>
-                            <b>Centres d'intérêt : </b> Je me passionne pour les nouvelles technologies, Je suis
-                            accro au
-                            numérique
-                        </p>
-                        <p>
-                            Le concepteur multimédia crée des produits cohérents en combinant textes, images, sons,
-                            vidéos et dessins. Il établit un scénario détaillé pour rendre le produit convivial et
-                            dirige le processus de création, collaborant avec divers professionnels, que ce soit en
-                            tant que salarié ou travailleur indépendant.
-                        </p>
-                    </div>
-                    <div class="carousel-slide">
-                        <img src="..\img\webmaster.jpeg" alt="Webmaster" width="200px">
-                        <h2>Webmaster</h2>
-                        <p>
-                            <b>Synonymes :</b> Administrateur/trice de site, Webmestre éditorial/e, Webmestre
-                            technique
-                            <br>
-                            <b>Secteurs professionnels :</b> Tout secteur nécéssitant un site web (Agriculture,
-                            Commerce et distribution, publicité, librairie,...)
-                            <br>
-                            <b>Centres d'intérêt :</b> J'ai le sens du contact, Je me passionne pour les nouvelles
-                            technologies, Je suis accro au numérique, Je veux travailler dans la communication
-                        </p>
-                        <p>
-                            À la fois technicien, graphiste et rédacteur, le webmestre est responsable de la vie
-                            d'un site, du développement à l'animation, en passant par la mise en ligne et la veille
-                            technologique. Un métier à géométrie variable nécessitant d'avoir plusieurs cordes à son
-                            arc.
-                        </p>
-                    </div>
-                    <div class="carousel-slide">
-                        <img src="..\img\ux-designer.jpeg" alt="UX designer" width="200px">
-                        <h2>UX designer</h2>
-                        <p>
-                            <b> Secteurs professionnels :</b> Édition, librairie, bibliothèque, Informatique et
-                            réseaux,
-                            Journalisme
-                            <br>
-                            <b>Centres d'intérêt :</b> Je me passionne pour les nouvelles technologies, Je suis
-                            accro au
-                            numérique
-                        </p>
-                        <p>
-                            L'UX designer améliore l'expérience des utilisateurs en simplifiant la navigation en
-                            ligne, en analysant les comportements des utilisateurs et en collaborant avec des
-                            équipes de développement multimédia, tout en travaillant étroitement avec l'UI designer
-                            pour la conception graphique
-                        </p>
-                    </div>
-                </div>
-                <button onclick="nextSlide()" class="arrows">
-                    >
-                </button>
+                    < </button>
+                        <div class="carousel-container">
+                            <div class="carousel-slide">
+                                <img src="..\img\mmi-logo-couleur_1.png" alt="Logo MMI" width="100px">
+                                <h2>Découvrir les débouchés</h2>
+                                <p>
+                                    Comme le BUT MMI permet d'acquérir énormément de compétences différentes, il permet
+                                    aussi de travailler dans toutes sortes de métiers et toutes sortes de domaines.
+                                </p>
+                                <p>
+                                    Les <b>génies créatifs</b>, pouront devenir <b>designer</b>, <b>infographiste</b>,
+                                    <b>game designer</b> ou <b>technicien audiovisuel</b>.
+                                </p>
+                                <p>
+                                    Pour les <b>rois du code</b>, ils ne sera pas difficile de rejoindre les <b>métiers
+                                        d’intégrateur</b>, de <b>développeur « back », « front », ou « full stack »</b>,
+                                    <b>d’intégrateur de dispositifs de réalité virtuelle</b>.
+                                </p>
+                                <p>
+                                    Les <b>maîtres de la communication</b>, quant à eux, pourraient se lancer en tant
+                                    que <b>chargé de communication numérique</b>, <b>UX designer</b>, <b>rédacteur
+                                        Web</b>,
+                                    <b>community manager</b>, <b>spécialiste du référencement sur Internet</b>.
+                                </p>
+                                <p>
+                                    C’est une formation très polyvalente qui permet d’accéder à beaucoup de compétences,
+                                    il y a donc une plus grande liberté et diversité de choix que ce soit dans les
+                                    études supérieurs ou la voix professionnels.
+                                </p>
+                                <h4>Parcourez les pages de ce caroussel et vous pourrez en découvir plus sur certains
+                                    métiers</h4>
+                            </div>
+
+                            <div class="carousel-slide">
+                                <img src="..\img\animateur-2d-3d.jpeg" alt="Animation 2D" width="200px">
+                                <h2>Animateur / animatrice 2D et 3D</h2>
+                                <p>
+                                    <b> Centres d'intérêt :</b> Passioné par le dessin et le dessin numériques, les
+                                    animation, les dessins animés
+                                </p>
+                                <p>
+                                    Dans son travail, l'animateur 2D commence par travailler au papier avant de passer
+                                    au numérique . En 3D par contre, l'animateur 3D commence en travaillant directement
+                                    sur ordinateur pour travailler avec les reliefs.
+                                    Il s'agit d'un travail long et minutieux. Il faut s'assurer que les mouvements
+                                    soient fluides et précis
+                                </p>
+                                <p>
+                                    Pour faire ce métier il est important d'avoir un bon niveau de dessin papier et de
+                                    dessin numérique. Il faut aussi bien connaître les différents logiciels d'animation
+                                    2D et/ou 3D.
+                                </p>
+                            </div>
+                            <div class="carousel-slide">
+                                <img src="..\img\concepteur-multimedia.jpeg" alt="Image 3" width="200px">
+                                <h2>Concepteur / conceptrice multimédia</h2>
+                                <p>
+                                    <b>Synonymes :</b> Concepteur/trice-réalisateur/trice multimédia
+                                    <br>
+                                    <b>Métiers Associés :</b> Concepteur/trice de jeux vidéo
+                                    <br>
+                                    <b>Secteurs professionnels :</b> Édition, librairie, bibliothèque, Informatique et
+                                    réseaux,
+                                    Journalisme, Marketing , publicité
+                                    <br>
+                                    <b>Centres d'intérêt : </b> Je me passionne pour les nouvelles technologies, Je suis
+                                    accro au
+                                    numérique
+                                </p>
+                                <p>
+                                    Le concepteur multimédia crée des produits cohérents en combinant textes, images,
+                                    sons,
+                                    vidéos et dessins. Il établit un scénario détaillé pour rendre le produit convivial
+                                    et
+                                    dirige le processus de création, collaborant avec divers professionnels, que ce soit
+                                    en
+                                    tant que salarié ou travailleur indépendant.
+                                </p>
+                            </div>
+                            <div class="carousel-slide">
+                                <img src="..\img\developpeur-informatique.jpeg" alt="Développeur informatique"
+                                    width="200px">
+                                <h2>Développeur / développeuse d'application mobile</h2>
+                                <p>
+                                    <b>Centres d'intérêt :</b> Soyez sûr d'aimer les nouvelles technologies et le
+                                    développement, car s'est un milieu en constante évolution qui nécessite que l'on se
+                                    tienne au courant de ces évolutions
+                                </p>
+                                <p>
+                                    Créer des applications mobiles est la principale mission du développeur
+                                    d'application mobile. Ce peut être des applications à créer de toute pièces ou bien
+                                    des des adaptation de site web ou même simplement optimiserune appication déjà
+                                    existante.
+                                </p>
+                                <p>
+                                    Vous aurez besoin d'un bon niveau de connaisance des normes et des langages de
+                                    programmation bien sûr, mais aussi en mathématiques (algorithmique, géométrie et
+                                    analyse)
+                                </p>
+                            </div>
+                            <div class="carousel-slide">
+                                <img src="..\img\webmaster.jpeg" alt="Webmaster" width="200px">
+                                <h2>Webmaster</h2>
+                                <p>
+                                    <b>Synonymes :</b> Administrateur/trice de site, Webmestre éditorial/e, Webmestre
+                                    technique
+                                    <br>
+                                    <b>Secteurs professionnels :</b> Tout secteur nécéssitant un site web (Agriculture,
+                                    Commerce et distribution, publicité, librairie,...)
+                                    <br>
+                                    <b>Centres d'intérêt :</b> J'ai le sens du contact, Je me passionne pour les
+                                    nouvelles
+                                    technologies, Je suis accro au numérique, Je veux travailler dans la communication
+                                </p>
+                                <p>
+                                    À la fois technicien, graphiste et rédacteur, le webmestre est responsable de la vie
+                                    d'un site, du développement à l'animation, en passant par la mise en ligne et la
+                                    veille
+                                    technologique. Un métier à géométrie variable nécessitant d'avoir plusieurs cordes à
+                                    son
+                                    arc.
+                                </p>
+                            </div>
+                            <div class="carousel-slide">
+                                <img src="..\img\ux-designer.jpeg" alt="UX designer" width="200px">
+                                <h2>UX designer</h2>
+                                <p>
+                                    <b> Secteurs professionnels :</b> Édition, librairie, bibliothèque, Informatique et
+                                    réseaux,
+                                    Journalisme
+                                    <br>
+                                    <b>Centres d'intérêt :</b> Je me passionne pour les nouvelles technologies, Je suis
+                                    accro au
+                                    numérique
+                                </p>
+                                <p>
+                                    L'UX designer améliore l'expérience des utilisateurs en simplifiant la navigation en
+                                    ligne, en analysant les comportements des utilisateurs et en collaborant avec des
+                                    équipes de développement multimédia, tout en travaillant étroitement avec l'UI
+                                    designer
+                                    pour la conception graphique
+                                </p>
+                            </div>
+                        </div>
+                        <button onclick="nextSlide()" class="arrows">
+                            >
+                        </button>
             </div>
             <br>
             <form action="../../php/navigation/update_progression.php" method="post">
